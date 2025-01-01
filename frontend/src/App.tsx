dc49b57edba982571af47748da1d522e4754fcb2
Paul Hovley <rphovley@gmail.com>
2025-01-01 10:17:11 -0700

    Add frontend structure

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <div>
+      <h1>🎉 Welcome to the Fullstack Challenge! 🎉</h1>
+      <p>Replace the content here with your own code and organize files as you see fit.</p>
+      <p>Good luck!</p>
+    </div>
+  )
+}
+
+export default App
